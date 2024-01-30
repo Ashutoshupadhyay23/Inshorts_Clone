c8c673f4da1c6c715f6154e6f70edc0e888b1629
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-01-30 20:01:21 +0530

    done with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import NewsContent from './components/NewsContent'
 import axios from 'axios'
+import apiKey from './data/Config'
 
 function App() {
   
   const [category, setCategory] = useState('general')
+  const [newsArray, setNewsArray] = useState([]);
+  const [newsResults, setNewsResults] = useState();
 
   const newsApi = async () => {
     try {
       const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&category=${category}`);
 
+      setNewsArray(news.data.articles)
+      setNewsResults(news.data.totalResults)
+
     } catch (error) {
       console.log('error', error)
     }
   }
+
+  // console.log(newsArray)
+
+  useEffect(() => {
+    newsApi();
+  }, [newsResults, category])
+  
 
   return (
     <div className='App'>
